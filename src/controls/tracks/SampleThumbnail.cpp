ea31ed755edd0a730f4030fe6504944421b70a6d
jfarr <jonothan.farr@gmail.com>
2022-03-06 18:21:27 -0800

    thumbnail alpha

--- src/controls/tracks/SampleThumbnail.cpp
+++ src/controls/tracks/SampleThumbnail.cpp
@@ -33,14 +33,16 @@ void SampleThumbnail::paintWithoutOverlay(juce::Graphics &g) {
     auto area = getLocalBounds();
     auto margin = 3;
 
-    g.fillAll(track.isSelected() && sample.isSelected() ? juce::Colours::lightgrey : juce::Colours::dimgrey);
+    auto alpha = 0.8f;
+    auto bgcolor = track.isSelected() && sample.isSelected() ? juce::Colours::lightgrey : juce::Colours::dimgrey;
+    g.fillAll(bgcolor.withAlpha(alpha));
     g.setColour(juce::Colours::grey);
     g.drawRect(0, 0, getWidth(), getHeight());
 
     auto thumbnailBounds = area.reduced(margin);
-    g.setColour(juce::Colours::darkgrey);
+    g.setColour(juce::Colours::darkgrey.withAlpha(alpha));
     g.fillRect(thumbnailBounds);
-    g.setColour(juce::Colours::green);
+    g.setColour(juce::Colours::green.withAlpha(alpha));
     thumbnail.drawChannels(g, thumbnailBounds, 0.0, thumbnail.getTotalLength(), 1.0f);
 }
 
